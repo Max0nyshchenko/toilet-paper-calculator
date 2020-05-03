65622358b991fbe5509ff5aa5c9dc14045d590ef
Max0nyshchenko <max.0nyshenk03@gmail.com>
2020-05-03 17:18:07 +0300

    bg rain fail  animation created

--- js/app.js
+++ js/app.js
@@ -70,3 +70,72 @@ $(function () {
   generateUsers();
   calculate();
 });
+
+// RAIN ANIMATION
+
+// class
+class Drop {
+  constructor(xPosition, yPosition, dropSpeed, dropWidth, dropHeight) {
+    this.xPosition = xPosition;
+    this.yPosition = yPosition;
+    this.dropSpeed = dropSpeed;
+    this.dropWidth = dropWidth;
+    this.dropHeight = dropHeight;
+    this.element;
+  }
+
+  show() {
+    this.element = document.createElement('div');
+    this.element.className += 'rainDrop';
+    this.element.style.top = this.yPosition + 'px';
+    this.element.style.left = this.xPosition + 'px';
+    this.element.style.width = this.dropWidth / 2 + 'px';
+    this.element.style.height = this.dropHeight + 'px';
+
+    let el = document.getElementById('drop-section');
+    el.appendChild(this.element);
+  }
+
+  fall() {
+    const makeItRain = () => {
+      this.yPosition += this.dropSpeed;
+      this.element.style.top = this.yPosition + 'px';
+
+      if (this.yPosition < window.innerHeight * 2) {
+        requestAnimationFrame(makeItRain);
+      } else {
+        this.yPosition = -10;
+        requestAnimationFrame(makeItRain);
+      }
+    };
+
+    requestAnimationFrame(makeItRain);
+  }
+}
+
+// complete rain
+const pageWidth = window.innerWidth;
+const pageHeight = window.innerHeight;
+const defaultDropNumber = 300;
+
+function makeItRain(num) {
+  let elments = document.getElementById('drop-section');
+
+  while (elments.hasChildNodes()) {
+    elments.removeChild(element.lastChild);
+  }
+
+  for (let i = 0; i < num; i++) {
+    let randomX = Math.floor(Math.random() * pageWidth);
+    let randomY = Math.floor(Math.random() * pageHeight);
+    let dropSpeed = Math.floor(Math.random() * (25 - 5)) + 1;
+    let dropWidht = Math.floor(Math.random() * (dropSpeed / 10 - 1)) + 1;
+    let dropHeight = Math.floor(Math.random() * (dropSpeed * 2 - 3)) + 3;
+    let d = new Drop(randomX, randomY, dropSpeed, dropWidht, dropHeight);
+
+    d.show();
+    d.fall();
+  }
+}
+
+makeItRain(defaultDropNumber);
